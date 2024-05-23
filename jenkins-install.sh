5f609d8f3d32618134e840aac4f561f52afe96e6
uduak <uduak@liontechacademy.com>
2024-05-23 09:16:50 +0100

    jd-k17.sh

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install default-jdk
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
